81405d97be19d2068abffeeb64d2853cfbec3625
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-11-28 12:52:56 +0200

    #Task1 Added exercise 1(Добавленно задание 1)

---
+++ src/Ex_1.java
@@ -0,0 +1,6 @@
+public class Ex_1 {
+    public static void main(String[] args) {
+        System.out.println("\"Your time is limited,\n"+
+                "   so don’t waste it\n\t   living someone else’s life\"\n\t\t   Steve Jobs");
+    }
+}
